60c609d2a1633f0253c9475ece75ab6913a69885
Kola1990 <kolumide40@gmail.com>
2022-09-10 20:36:43 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+*
+/**
+ * main - print the string in the main function
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of a char: %1d byte(s)\n", sizeof(c));
+	printf("sixe of an int: %1d byte(s)\n", sizeof(i));
+	printf("size of a long int : %1d byte(s)\n", sizeof(li));
+	printf("size of a long long int: %1d byte(s)\n", sizeof(lli));
+	printf("size of a float: %1d byte(s)\n", sizeoff(f));	
+	return (0);
+}	
