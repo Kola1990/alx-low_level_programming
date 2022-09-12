ee44fd29380a5fcf7d31a234c9946a614d585606
Kola1990 <kolumide40@gmail.com>
2022-09-12 16:25:48 -0700

    correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0 (success)
  */
-int main(void)
-{
-	puts("\"programming is like building a multi lingual puzzle\n");
+int main(void)	
+{	puts("\"programming is like building a multi lingual puzzle\n");
 	return(0);
 }
 
